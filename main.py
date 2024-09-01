1c3329be564266b14aee7a8273356ba10323c296
Banana2048 <146014034+Banana2048@users.noreply.github.com>
2024-09-01 13:35:30 -0700

    user can enter file path in the command line

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from pathlib import Path
 from file_handling import read_lines_from_file, process_test
 
 def main():
-    file_path = Path("test_file.txt") # input the desired text file to convert here
+    file_path = Path(input("Enter path to test question file: "))
+
+    if not file_path.is_file():
+        print("File not found.")
+        return
+
     test = process_test(read_lines_from_file(file_path))
     test.shuffle_questions()
     test.display_test()
